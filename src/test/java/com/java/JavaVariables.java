9e85328e92798d16a949bb865b9e969f859708f7
Naga Venkata Raghuveer Chebrolu <raghuchnv@gmail.com>
2023-04-04 07:24:53 +0530

    4 march 2023 java basics

--- src/test/java/com/java/JavaVariables.java
+++ src/test/java/com/java/JavaVariables.java
@@ -1,9 +1,9 @@
 package com.java;
 
 public class JavaVariables {
-	int a =10; //instance variables : declared inside a class but outside a method
+	int a; //instance variables : declared inside a class but outside a method
 	static int b=9; // static variables declared with help of static keyword
-	
+	boolean flag =true;
 	//local variables : variables declared inside a method is called as local variables
 	 int add(int i, int j) {     
 		//System.out.println(i+j);
@@ -13,6 +13,7 @@ public class JavaVariables {
 	public static void main(String[] args) {
 		JavaVariables obj = new JavaVariables();
 		System.out.println(obj.a);
+		System.out.println(obj.flag);
 		System.out.println(b);
 		System.out.println(obj.add(4,8));
 	}
